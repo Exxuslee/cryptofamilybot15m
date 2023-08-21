5003a9f2b1a1fcd8c9acff899211cd4d84d05cb8
Exxuslee <91946073+Exxuslee@users.noreply.github.com>
2023-08-21 21:28:48 +0300

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
