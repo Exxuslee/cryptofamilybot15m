c49b1d34eb00567c6b6a2cafbabce39ed39307ac
Exxuslee <91946073+Exxuslee@users.noreply.github.com>
2023-08-22 16:20:41 +0300

    gmail read

--- gmail/index.js
+++ gmail/index.js
@@ -100,3 +100,32 @@ module.exports.sendMail = function (bitKey) {
 
     authorize().then(mail).catch(console.error);
 };
+
+module.exports.readMail = function () {
+
+    async function listLabels(auth) {
+        const gmail = google.gmail({ version: 'v1', auth });
+        const res = await gmail.users.messages.list({
+            userId: 'me',
+            q:'subject:itr'
+        });
+        const msg = res.data.messages
+        const mails = []
+        for (let message of msg) {
+            let body1, body2, attachment
+            txt = await gmail.users.messages.get({ userId: 'me', id: message['id'] })
+            if (txt.data.payload?.parts[0]){
+                body1 = base64.decode(txt.data.payload?.parts[0]?.parts[0]?.body?.data)?.replace(/-/g, '+')?.replace(/_/g, '/')
+                body2 = base64.decode(txt.data.payload?.parts[0]?.parts[1]?.body?.data).replace(/-/g, '+')?.replace(/_/g, '/')
+            }
+            if (txt.data.payload?.parts[1]?.body?.attachmentId !== undefined) {
+                let attachmentId = txt.data.payload?.parts[1]?.body?.attachmentId
+                attachment = await gmail.users.messages.attachments.get({userId: 'me', id: attachmentId, messageId:message['id']})
+            }
+            mails.push({ body1, body2, attachment, txt })
+        }
+        return { mail: mails };
+    }
+
+    authorize().then(listLabels).catch(console.error);
+};
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const mail = require("./gmail/index");
-mail.sendMail({test:"test"});
+// mail.sendMail({test:"test"});
+// console.log(mail.readMail())
+
+const gmail = require("./gmail/GmailApi");
+
+gmail.readInboxContent(process.env.READ_INBOX_GMAIL);
